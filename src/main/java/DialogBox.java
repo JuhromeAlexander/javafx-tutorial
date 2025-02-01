c8be315cd861f328257f11052b209a9c56d55aa0
Muhd Juraimi <muhd.juraimi123@gmail.com>
2025-02-01 23:47:36 +0800

    feat: iteration 3 java-fx part 3

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -3,7 +3,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.image.ImageView;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 
 public class DialogBox extends HBox {
 
@@ -21,5 +23,22 @@ public class DialogBox extends HBox {
 
         this.getChildren().addAll(text, displayPicture);
 
+    }
+
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> temp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(temp);
+        this.getChildren().setAll(temp);
+    }
+
+    public static DialogBox getUserDialog(String s, Image i) {
+        return new DialogBox(s, i);
+    }
+
+    public static DialogBox getDukeDialog(String s, Image i) {
+        var db = new DialogBox(s, i);
+        db.flip();
+        return db;
     }
 }
